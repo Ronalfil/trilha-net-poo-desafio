f4824b8c7b85fd9057f2aa37bd38576fd86bee78
Ronaldo Filho <ronaldonfilho22@hotmail.com>
2024-08-13 12:23:58 -0300

    Feito Celulares

--- Program.cs
+++ Program.cs
@@ -1,3 +1,81 @@
-﻿using DesafioPOO.Models;
+﻿using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace Celulares
+{
+    // Classe abstrata Smartphone
+    public abstract class Smartphone
+    {
+        public string Numero { get; private set; }
+        public string Modelo { get; private set; }
+        public string IMEI { get; private set; }
+        public int Memoria { get; private set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        // Método abstrato que será implementado nas classes filhas
+        public abstract void InstalarAplicativo(string nomeApp);
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+    }
+
+    // Classe Nokia que herda de Smartphone
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia...");
+        }
+    }
+
+    // Classe Iphone que herda de Smartphone
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone...");
+        }
+    }
+
+    // Classe Program para executar o código
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Criando um objeto Nokia
+            Smartphone nokia = new Nokia("123456789", "Nokia Model", "111111111111111", 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+            // Criando um objeto Iphone
+            Smartphone iphone = new Iphone("987654321", "iPhone Model", "222222222222222", 128);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
